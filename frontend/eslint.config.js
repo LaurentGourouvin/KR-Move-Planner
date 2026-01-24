7f80214e2ccbbe96ff7b8e32a937272e8b7e32f2
Eva Demmer <eva.demmer@gmail.com>
2026-01-24 16:01:03 +0100

    Setup imports config

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -5,6 +5,24 @@ import nextTs from "eslint-config-next/typescript";
 export default defineConfig([
   ...nextVitals,
   ...nextTs,
+  {
+    parser: "@typescript-eslint/parser",
+    plugins: ["@typescript-eslint"],
+    parserOptions: {
+      ecmaVersion: 2020,
+      sourceType: "module",
+      ecmaFeatures: { jsx: true },
+      project: "./tsconfig.json",
+      tsconfigRootDir: __dirname
+    },
+    settings: {
+      "import/resolver": {
+        typescript: {
+          project: "./tsconfig.json"
+        }
+      }
+    }
+  },
   globalIgnores([
     // Default ignores of eslint-config-next:
     "node_modules/**",
