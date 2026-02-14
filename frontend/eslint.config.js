9b7377c8b6bfaef461bfb6efc3ef263154627114
Eva Demmer <eva.demmer@gmail.com>
2026-02-14 18:56:13 +0100

    setup style

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,18 +1,29 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import tseslint from "typescript-eslint";
+import prettier from "eslint-config-prettier";
 
 export default defineConfig([
   ...nextVitals,
   ...nextTs,
+  prettier,
+  tseslint.configs.recommendedTypeChecked,
   {
     settings: {
       "import/resolver": {
         typescript: {
-          project: "./tsconfig.json"
-        }
-      }
-    }
+          project: "./tsconfig.json",
+        },
+      },
+    },
+  },
+  {
+    languageOptions: {
+      parserOptions: {
+        project: true,
+      },
+    },
   },
   globalIgnores([
     // Default ignores of eslint-config-next:
